b7ddfa0d60091bf1f73cab0a6a85d641c8b50808
NadiaAiraf <aidanfaria@gmail.com>
2018-10-08 17:41:18 +0100

    end of day 1, still stuck on sessions

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 
 app.set('view engine','ejs')
+app.use(cookieParser());
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
 
-app.get('/', (req, res) => res.render('index'))
+app.get('/', (req, res) => res.render('index'));
 
 app.get('/hi', function(req, res) {
-  console.log('hello');
   res.render('signup')
+  req.session.user = 'hello';
+  console.log(req.session);
 })
+
+
 
 app.get('/anything', function(req, res) {
   res.redirect('/hi');
+  console.log(req.session);
 })
 
 app.use(express.static('public'))
