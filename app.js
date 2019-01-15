124c55519d5e15660d77ed3d5228c61bc777c907
Emma Albury <emma_albury@hotmail.com>
2019-01-15 19:40:33 +0000

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/views'));
 
-mongoose.connect('mongodb://localhost/27017');
+mongoose.connect('mongodb://localhost/27017', { useNewUrlParser: true });
 
 
 app.get('/', (req, res) => res.render('index',{session: req.session}));
